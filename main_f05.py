3c9470aab87cd7743efd0392b96f937846564d21
Technoranti <ranting.tech@gmail.com>
2024-12-20 11:20:57 +0800

    update to allow for user input of test name and file path

--- main_f05.py
+++ main_f05.py
@@ -1,6 +1,8 @@
 import AFA_eval_functions as AFA
 
 data = list()
+#test_name = input("Please enter the name of the test: ")               #uncomennt this to unlock user input for test name
+#file_path = input("Please enter the file path of the test data: ")     #uncomennt this to unlock user input for file path
 evaluation_record = AFA.start_new_record("test_run")
 response_list = AFA.csv_to_list_of_dicts("Dataset/AFA_BulkEval_Test1.csv")
 
